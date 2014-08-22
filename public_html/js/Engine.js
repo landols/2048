3b908657df1c569f0e0dc3e86046656cee2bf514
Mario <mario.landols@gmail.com>
2014-08-22 12:21:59 +0200

    inizio del gioco

--- public_html/js/Engine.js
+++ public_html/js/Engine.js
@@ -1,23 +1,56 @@
 var Engine = new Class({
     container: null,
     pieces: [],
+    colourClasses: {
+        2: "one",
+        4: "two",
+        8: "three",
+        16: "four",
+        32: "five",
+        64: "six",
+        128: "seven",
+        256: "eight",
+        512: "nine",
+        1024: "ten",
+        2048: "final"
+    },
     initialize: function(container_id) {
         this.container = document.id(container_id);
+        this.pieces.push(this.createPiece());
         this.pieces.push(this.createPiece());
 
         this.printPieces();
     },
     createPiece: function() {
+        var check, x, y;
+
+        do {
+            x = $random(0, 3);
+            y = $random(0, 3);
+
+            check = this._check_piece_position(x, y);
+        }
+        while (!check);
+
         return {
             value: $random(1, 2) * 2,
-            x: $random(0, 3),
-            y: $random(0, 3)
+            x: x,
+            y: y
         };
+    },
+    _check_piece_position: function(x, y) {
+        for (i = 0; i < this.pieces.length; i++) {
+            if ((this.pieces[i].x == x) && (this.pieces[i].y == y)) {
+                return false;
+            }
+        }
+
+        return true;
     },
     printPieces: function() {
         this.pieces.each(function(piece) {
             var pieceElement = new Element("div", {
-                class: "piece",
+                class: "piece " + this.colourClasses[piece.value],
                 html: piece.value,
                 styles: {
                     top: piece.y * 100,
@@ -26,6 +59,6 @@ var Engine = new Class({
             });
 
             pieceElement.inject(this.container);
-        });
+        }.bind(this));
     }
 });
