1e72172e049d27d758d6b3a3cdc46bc5c9eb8049
Mario <mario.landols@gmail.com>
2014-08-22 10:02:38 +0200

    creazione primo pezzo e stile

--- public_html/js/Engine.js
+++ public_html/js/Engine.js
@@ -1,3 +1,31 @@
 var Engine = new Class({
-    
+    container: null,
+    pieces: [],
+    initialize: function(container_id) {
+        this.container = document.id(container_id);
+        this.pieces.push(this.createPiece());
+
+        this.printPieces();
+    },
+    createPiece: function() {
+        return {
+            value: $random(1, 2) * 2,
+            x: $random(0, 3),
+            y: $random(0, 3)
+        };
+    },
+    printPieces: function() {
+        this.pieces.each(function(piece) {
+            var pieceElement = new Element("div", {
+                class: "piece",
+                html: piece.value,
+                styles: {
+                    top: piece.y * 100,
+                    left: piece.x * 100
+                }
+            });
+
+            pieceElement.inject(this.container);
+        });
+    }
 });
